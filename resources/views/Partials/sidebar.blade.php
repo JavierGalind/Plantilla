63a1855a0d6740a0bbe5a3d6e5d822f918ff7f32
Luis Javier Galindo Guadarra,ma <luis.galindo297@gmail.com>
2019-08-28 18:39:46 -0500

    ver submenus

--- resources/views/Partials/sidebar.blade.php
+++ resources/views/Partials/sidebar.blade.php
@@ -24,23 +24,40 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="/adminlte/index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-            </ul>
-          </li>
+               <li class="nav-item has-treeview menu-open">
+                  <a href="#" class="nav-link active">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                      CANTO CONTADORES
+                      <i class="right fas fa-angle-left"></i>
+                    </p>
+                  </a>
+                  <ul class="nav nav-treeview">
+                    <li class="nav-item ">
+                      <a href="/adminlte/index.html" class="nav-link active">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Dashboard v1</p>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item has-treeview menu-open">
+                    <a href="#" class="nav-link active">
+                      <i class="nav-icon fas fa-tachometer-alt"></i>
+                      <p>
+                        INCRETEC
+                        <i class="right fas fa-angle-left"></i>
+                      </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                      <li class="nav-item has-treeview">
+                        <a href="/adminlte/index.html" class="nav-link active">
+                          <i class="far fa-circle nav-icon"></i>
+                          <p>Dashboard v1</p>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
